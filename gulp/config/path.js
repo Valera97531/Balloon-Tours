7ab3438a33454ebe2df9d7ffdd45202a12d836f6
Valera97531 <10002000valera@gmail.com>
2022-10-30 17:51:21 +0200

    added two section

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -11,23 +11,23 @@ export const path = {
 		html: `${buildFolder}/`,
 		images: `${buildFolder}/img/`,
 		fonts: `${buildFolder}/fonts/`,
-		files: `${buildFolder}/files/`
+		files: `${buildFolder}/files/`,
 	},
 	src: {
 		js: `${srcFolder}/js/app.js`,
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,webp,mp4}`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,webp,mp4,eot,ttf,woff}`,
 		svg: `${srcFolder}/img/**/*.svg`,
 		scss: `${srcFolder}/scss/style.scss`,
 		html: `${srcFolder}/*.{html,php,pug}`,
 		files: `${srcFolder}/files/**/*.*`,
-		svgicons: `${srcFolder}/svgicons/*.svg`
+		svgicons: `${srcFolder}/svgicons/*.svg`,
 	},
 	watch: {
 		js: `${srcFolder}/js/**/*.js`,
 		scss: `${srcFolder}/scss/**/*.scss`,
 		html: `${srcFolder}/**/*.{html,php,pug}`,
-		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp,mp4}`,
-		files: `${srcFolder}/files/**/*.*`
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp,mp4,eot,ttf,woff}`,
+		files: `${srcFolder}/files/**/*.*`,
 	},
 	clean: buildFolder,
 	buildFolder: buildFolder,
